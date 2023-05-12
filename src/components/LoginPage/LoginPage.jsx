a705a10ffb3dadadb98719eb0ef54eba51895946
Tristanslayer <tristansibug@gmail.com>
2023-05-13 05:56:45 +0800

    easiest as sht

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { CgGoogle } from 'react-icons/cg';
 import { useNavigate } from 'react-router-dom';
 
@@ -14,33 +15,46 @@ const LoginPage = () => {
     event.preventDefault();
     // Handle login logic here
   };
-     return (
-        <div className="login-page">
-          <div className="login-form">
-            <p className="welcome">Welcome to LabTICA!</p>
-            <p className="login-description">Please log in to proceed to your CBC test results and personalized health recommendations. Our secure platform ensures your privacy and confidentiality.</p>
-            <button className="btn-login">
-                  <CgGoogle />Continue with Google
-            </button>
-            <div className="or-wrapper">
-              <hr className="line1" />
-              <p className="or">Or</p>
-              <hr className="line2" />
+  return (
+    <div class="container-fluid">
+      <p class="welcome">Welcome to LabTICA!</p>
+      <p class="login-description">Please log in to proceed to your CBC test results and personalized health</p>
+      <p class="login-description">recommendations. Our secure platform ensures your privacy and confidentiality.</p>
+
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <button class="btn-login border border-0 rounded-5">
+            <CgGoogle />Continue with Google</button>
+        </div>
+      </div>
+
+      <div class="row mt-4"></div>
+      <div class="col d-flex justify-content-center">
+        <hr class="line1 border border-1 opacity-100" />
+        <p class="or mx-3">Or</p>
+        <hr class="line1 border border-1 opacity-100" />
+      </div>
+
+
+      <div class="row mt-2">
+        <div class="col d-flex justify-content-center">
+          <button class="btn-signup border border-0 rounded-5" onClick={() => {
+            navigate('/signin');
+          }}>
+            Sign Up
+            <div class="arrow-wrapper">
+              <div class="arrow"></div>
             </div>
-              <button className="btn-signup" onClick={()=>{
-        navigate('/signin');
-      }}>
-              Sign Up
-                <div class="arrow-wrapper">
-                  <div class="arrow"></div>
-                </div>
-                </button>
-            </div>
-            <div className="signup-link">
-              <p>Don't have an account yet?</p>
-            </div>
-          </div>
-      );
+          </button>
+        </div>
+      </div>
+
+      <div class="signup-link mt-4">
+        <p>Don't have an account yet?</p>
+      </div>
+
+    </div>
+  );
 };
 
 export default LoginPage;
